c83e44f3793d5f1707f3dc067043a4fe41d01d45
lulu <lulujiang505@gmail.com>
2024-11-22 19:53:58 -0800

    refined index.html and hosted in firebase

--- scripts/skeleton.js
+++ scripts/skeleton.js
@@ -16,7 +16,7 @@ function loadSkeleton() {
 
     firebase.auth().onAuthStateChanged(function (user) {
         if (user) {                   
-		        // If the "user" variable is not null, then someone is logged in
+		    // If the "user" variable is not null, then someone is logged in
             // User is signed in.
             // Do something for the user here.
             console.log($('#navbarPlaceholder').load('./text/nav_after_login.html'));
